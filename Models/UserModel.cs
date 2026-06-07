990583a9293b5e4a823e0b5335a31de6f80a232e
itsAishbaa <aalishbaa3@gmail.com>
2026-06-07 10:46:29 -0700

    adding code in file

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,6 +1,19 @@
-﻿namespace q2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
 {
     public class UserModel
     {
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(20, ErrorMessage = "First name is too long.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(20, ErrorMessage = "Last name is too long.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = string.Empty;
     }
 }
